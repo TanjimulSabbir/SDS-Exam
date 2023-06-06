b448fd7ecca294059901627b31d0acd3329838bc
Abdur Rahman Apu <abdurrahmany418@gmail.com>
2023-06-07 02:31:54 +0600

    Nabar is created upper part

--- src/Pages/Navbar/Navbar.jsx
+++ src/Pages/Navbar/Navbar.jsx
@@ -1,76 +1,87 @@
-import React from "react";
+import Nav from "./Navbar.module.css";
+import Logo from "../../assets/Logo/logo.jpg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faClock,
+  faCoffee,
+  faLocationDot,
+  faPhone,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
   return (
-    <div className="navbar bg-base-100">
-      <div className="navbar-start">
-        <div className="dropdown">
-          <label tabIndex={0} className="btn btn-ghost lg:hidden">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h8m-8 6h16"
-              />
-            </svg>
-          </label>
-          <ul
-            tabIndex={0}
-            className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
-          >
-            <li>
-              <a>Item 1</a>
-            </li>
-            <li>
-              <a>Parent</a>
-              <ul className="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <a>Item 3</a>
-            </li>
-          </ul>
+    <div className={`${Nav.container}`}>
+      {/* upper part  */}
+      <div className={`${Nav.upper} flex items-center justify-between mb-2`}>
+        <div className={`${Nav.logo} basis-1/2 flex items-center`}>
+          <img src={Logo} alt="Logo" />
+
+          {/* company name  */}
+          <div className="flex flex-col ml-2">
+            <p className="font-bold text-2xl">SDS</p>
+            <span className="text-sm text-gray-400">Consultancy Service</span>
+          </div>
         </div>
-        <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+
+        <div className="flex justify-between basis-1/2">
+          {/* call info  */}
+          <div className="flex">
+            <div className="mr-2">
+              <FontAwesomeIcon icon={faPhone} />
+            </div>
+            <div className="flex flex-col">
+              <p className="font-bold text-sm">Call</p>
+              <span className="text-xs mt-1 text-gray-500">+44-7689789898</span>
+            </div>
+          </div>
+
+          {/* work time info  */}
+          <div className="flex ">
+            <div className="mr-2">
+              <FontAwesomeIcon icon={faClock} />
+            </div>
+            <div className="flex flex-col">
+              <p className="font-bold text-sm">Work Time</p>
+              <span className="text-xs mt-1 text-gray-500">
+                Mon-Fri 8AM -5PM
+              </span>
+            </div>
+          </div>
+
+          {/* address info  */}
+          <div className="flex">
+            <div className="mr-2">
+              <FontAwesomeIcon icon={faLocationDot} />
+            </div>
+            <div className="flex flex-col">
+              <p className="font-bold text-sm">Address</p>
+              <span className="text-xs mt-1 text-gray-500">
+                Franklin st. Avenue
+              </span>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">
+      <hr />
+
+      {/* bottom part  */}
+
+      <div>
+        <ul>
           <li>
-            <a>Item 1</a>
-          </li>
-          <li tabIndex={0}>
-            <details>
-              <summary>Parent</summary>
-              <ul className="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </details>
+            <a href="#">Home</a>
           </li>
           <li>
-            <a>Item 3</a>
+            <a href="#">Courses</a>
+          </li>
+          <li>
+            <a href="#">About us</a>
           </li>
         </ul>
-      </div>
-      <div className="navbar-end">
-        <a className="btn">Button</a>
+
+        <div>
+          <button>LogIn</button>
+        </div>
       </div>
     </div>
   );
