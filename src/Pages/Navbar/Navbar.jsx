cf659406329729150351bd0b5228961a986a2690
Abdur Rahman Apu <abdurrahmany418@gmail.com>
2023-06-07 05:59:20 +0600

    Navbar is completed

--- src/Pages/Navbar/Navbar.jsx
+++ src/Pages/Navbar/Navbar.jsx
@@ -1,33 +1,43 @@
 import Nav from "./Navbar.module.css";
 import Logo from "../../assets/Logo/logo.jpg";
+import {
+  faFacebook,
+  faFacebookF,
+  faInstagram,
+  faLinkedin,
+  faTwitter,
+} from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faClock,
-  faCoffee,
   faLocationDot,
   faPhone,
 } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
   return (
-    <div className={`${Nav.container}`}>
+    <div className={`${Nav.container} shadow-md`}>
       {/* upper part  */}
-      <div className={`${Nav.upper} flex items-center justify-between mb-2`}>
-        <div className={`${Nav.logo} basis-1/2 flex items-center`}>
+      <div
+        className={`${Nav.upper}  flex  md:items-center md:justify-between mb-2`}
+      >
+        <div
+          className={`${Nav.logo} md:basis-1/2 flex justify-center items-center md:justify-start `}
+        >
           <img src={Logo} alt="Logo" />
 
           {/* company name  */}
           <div className="flex flex-col ml-2">
-            <p className="font-bold text-2xl">SDS</p>
+            <p className={`font-bold text-2xl ${Nav.highlight}`}>SDS</p>
             <span className="text-sm text-gray-400">Consultancy Service</span>
           </div>
         </div>
 
-        <div className="flex justify-between basis-1/2">
+        <div className="md:flex justify-between basis-1/2 hidden ">
           {/* call info  */}
           <div className="flex">
             <div className="mr-2">
-              <FontAwesomeIcon icon={faPhone} />
+              <FontAwesomeIcon className="text-[#33d9b2]" icon={faPhone} />
             </div>
             <div className="flex flex-col">
               <p className="font-bold text-sm">Call</p>
@@ -38,7 +48,7 @@ const Navbar = () => {
           {/* work time info  */}
           <div className="flex ">
             <div className="mr-2">
-              <FontAwesomeIcon icon={faClock} />
+              <FontAwesomeIcon className="text-[#33d9b2]" icon={faClock} />
             </div>
             <div className="flex flex-col">
               <p className="font-bold text-sm">Work Time</p>
@@ -51,7 +61,10 @@ const Navbar = () => {
           {/* address info  */}
           <div className="flex">
             <div className="mr-2">
-              <FontAwesomeIcon icon={faLocationDot} />
+              <FontAwesomeIcon
+                className="text-[#33d9b2]"
+                icon={faLocationDot}
+              />
             </div>
             <div className="flex flex-col">
               <p className="font-bold text-sm">Address</p>
@@ -66,21 +79,71 @@ const Navbar = () => {
 
       {/* bottom part  */}
 
-      <div>
-        <ul>
-          <li>
-            <a href="#">Home</a>
-          </li>
-          <li>
-            <a href="#">Courses</a>
-          </li>
-          <li>
-            <a href="#">About us</a>
-          </li>
-        </ul>
+      <div className="navbar bg-base-100">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <label tabIndex={0} className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </label>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              <li>
+                <a>Home</a>
+              </li>
+              <li>
+                <a>Certifications</a>
+              </li>
+              <li>
+                <a>About Us</a>
+              </li>
+            </ul>
+          </div>
+          {/* <a className="btn btn-ghost normal-case text-xl">daisyUI</a> */}
 
-        <div>
-          <button>LogIn</button>
+          <div className="hidden lg:flex">
+            <ul className="menu menu-horizontal px-1 text-base font-semibold">
+              <li>
+                <a>Home</a>
+              </li>
+              <li>
+                <a>Certifications</a>
+              </li>
+              <li>
+                <a>About Us</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="navbar-end">
+          <div
+            className={`hidden w-[120px] md:flex justify-between mr-4 ${Nav.socialIcon}`}
+          >
+            <a href="#">
+              <FontAwesomeIcon icon={faFacebookF} />
+            </a>
+            <a href="#">
+              <FontAwesomeIcon icon={faLinkedin} />
+            </a>
+            <a href="#">
+              <FontAwesomeIcon icon={faInstagram} />
+            </a>
+          </div>
+          <a className={`btn text-white ${Nav.gradientBg}`}>LogIn</a>
         </div>
       </div>
     </div>
