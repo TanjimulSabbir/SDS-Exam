1a1f4b847f4bbcda97b7fe91281005a6ccbb2e7b
Abdur Rahman Apu <abdurrahmany418@gmail.com>
2023-06-07 17:40:28 +0600

    Banner is changed

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -5,7 +5,10 @@ const Banner = () => {
   return (
     <div className="hero lg:min-h-16 px-[6%] py-14">
       <div className="hero-content flex-col-reverse lg:flex-row-reverse">
-        <div className="basis-1/2  my-7 md:my-0">
+        <div className="basis-1/2  my-7 md:my-0 relative">
+          <div className="w-[80px] h-[80px] opacity-40 rounded-full bg-[#7bed9f]  absolute top-0 right-0"></div>
+          <div className="w-[20px] h-[20px] opacity-40 rounded-full bg-[#7bed9f]  absolute top-20 right-16"></div>
+          <div className="w-[30px] h-[30px] opacity-40 rounded-full bg-[#eccc68]  absolute bottom-20 left-16"></div>
           <img src={BannerImg} className=" md:max-w-sm md:mx-auto rounded-lg" />
         </div>
         <div className="basis-1/2">
@@ -15,7 +18,7 @@ const Banner = () => {
           </h1>
           <p className="py-6">
             Learning is very important in this modern era. Your confidence will
-            be increased, If you sit for an exam. You will learn
+            be increased, If you sit for an exam. You can judge yourself.
           </p>
           <button className="btn hover:bg-[#42BEC3] bg-[#42BEC3] border-0 text-white">
             Get Started
