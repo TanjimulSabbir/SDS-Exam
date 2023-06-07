4c7b07a44829f48c2fc95e41e92adeea2f1a0d8e
Abdur Rahman Apu <abdurrahmany418@gmail.com>
2023-06-08 00:10:07 +0600

    Redirects file and login page form validation is done

--- src/Pages/LogIn/LogIn.jsx
+++ src/Pages/LogIn/LogIn.jsx
@@ -1,9 +1,20 @@
+import { useForm } from "react-hook-form";
 import Logo from "../../assets/Logo/logo.jpg";
 import "../../assets/Css/Common.css";
 import Lottie from "lottie-react";
 import LoginAnimation from "../../assets/LottieFiles/login.json";
 
 const LogIn = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
     <div className="hero min-h-screen my-10">
       <div className="hero-content w-[85%] flex-col lg:flex-row justify-between">
@@ -27,7 +38,8 @@ const LogIn = () => {
             </div>
 
             {/* login fields  */}
-            <div className="my-4">
+            <form className="my-4" onSubmit={handleSubmit(onSubmit)}>
+              {/* registration id field  */}
               <div className="form-control">
                 <label className="label">
                   <span className="label-text font-bold">Registration ID</span>
@@ -36,24 +48,43 @@ const LogIn = () => {
                   type="text"
                   placeholder="Enter registration id"
                   className="input input-bordered"
+                  {...register("regId", { required: true })}
+                  aria-invalid={errors.regId ? "true" : "false"}
                 />
+
+                {errors.regId?.type === "required" && (
+                  <p role="alert" className="my-1 text-red-600">
+                    Registration ID is required
+                  </p>
+                )}
               </div>
-              <div className="form-control">
+
+              {/* password field  */}
+              <div className="form-control my-4 relative">
                 <label className="label">
                   <span className="label-text font-bold">Password</span>
                 </label>
                 <input
-                  type="text"
+                  type="password"
                   placeholder="password"
                   className="input input-bordered"
+                  {...register("password", { required: true })}
+                  aria-invalid={errors.password ? "true" : "false"}
                 />
+                {errors.regId?.type === "required" && (
+                  <p role="alert" className="my-1 text-red-600">
+                    password is required
+                  </p>
+                )}
               </div>
               <div className="form-control mt-6">
-                <button className="btn btn-primary button-bg text-white">
-                  Login
-                </button>
+                <input
+                  type="submit"
+                  value="LogIn"
+                  className="btn button-bg text-white"
+                />
               </div>
-            </div>
+            </form>
           </div>
         </div>
       </div>
