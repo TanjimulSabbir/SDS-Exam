db9eb836acf846e19a39611dffd6519c17b6ca8a
Abdur Rahman Apu <abdurrahmany418@gmail.com>
2023-06-08 14:26:34 +0600

    certification error handling is done

--- src/Pages/Certification/Certification.jsx
+++ src/Pages/Certification/Certification.jsx
@@ -20,11 +20,18 @@ const Certification = () => {
       </div>
 
       {/* certifications  */}
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-16">
-        {courses?.map((course) => (
-          <IndividualCertification key={course.courseId} course={course} />
-        ))}
-      </div>
+
+      {courses ? (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-16">
+          {courses?.map((course) => (
+            <IndividualCertification key={course.courseId} course={course} />
+          ))}
+        </div>
+      ) : (
+        <p className="text-center text-2xl font-bold my-6 text-red-500">
+          No courses are available right now
+        </p>
+      )}
     </div>
   );
 };
