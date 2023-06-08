fc8f9bb75b96658cc9ff10de225ce3bae40429df
Abdur Rahman Apu <abdurrahmany418@gmail.com>
2023-06-08 23:41:58 +0600

    Admin dashboard active menu is completed

--- src/Layout/DashboardLayout.jsx
+++ src/Layout/DashboardLayout.jsx
@@ -1,10 +1,17 @@
 import { useContext } from "react";
+import { MdDashboard } from "react-icons/md";
+import { AiFillProfile } from "react-icons/ai";
+import { BsPersonFillAdd, BsFillPeopleFill } from "react-icons/bs";
 import Navbar from "../Pages/Navbar/Navbar";
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet, useLocation } from "react-router-dom";
 import { AuthContext } from "../Context/AuthProvider";
+import style from "../Layout/DashboardLayout.module.css";
 
 const DashboardLayout = () => {
   const { employeeInfo } = useContext(AuthContext);
+
+  //   get pathname to highlight active menu
+  const pathName = useLocation().pathname;
 
   let menus;
 
@@ -12,13 +19,30 @@ const DashboardLayout = () => {
     menus = (
       <>
         <li>
-          <Link to="/dashboard">Dashboard</Link>
+          <Link
+            to="/dashboard"
+            className={pathName === "/dashboard" ? `${style.activeMenu}` : ""}
+          >
+            <MdDashboard /> Dashboard
+          </Link>
         </li>
         <li>
-          <a>Add Employee</a>
+          <Link>
+            {" "}
+            <AiFillProfile />
+            Profile
+          </Link>
         </li>
         <li>
-          <a>Employees</a>
+          <Link>
+            {" "}
+            <BsPersonFillAdd /> Add Employee
+          </Link>
+        </li>
+        <li>
+          <Link>
+            <BsFillPeopleFill /> Employees
+          </Link>
         </li>
       </>
     );
@@ -54,7 +78,9 @@ const DashboardLayout = () => {
         </div>
         <div className="drawer-side">
           <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
-          <ul className="menu p-4 w-80 h-full bg-base-200 text-base-content">
+          <ul
+            className={`menu p-4 w-80 h-full bg-[#30b6bc80] text-base-content text-base font-roboto ${style.dashboardMenu}`}
+          >
             {/* Sidebar content here */}
             {menus}
           </ul>
