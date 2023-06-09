e0b931258f4597d5b9a1ce4cd9bffa5fd5a1879b
Abdur Rahman Apu <abdurrahmany418@gmail.com>
2023-06-09 08:11:40 +0600

    A message is given in the dashboard page

--- src/Pages/Dashboard/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard/Dashboard.jsx
@@ -1,7 +1,9 @@
 const Dashboard = () => {
   return (
     <div>
-      <h1>Dashboard</h1>
+      <h1 className="text-center my-10 font-roboto text-4xl font-bold">
+        Welcome to the Dashboard
+      </h1>
     </div>
   );
 };
